308b3005bc30405b74bc6ac2792062d568dfaa0d
Mr_Happy <dvdvorle@gmail.com>
2010-01-03 20:44:42 +0800

    Added the OptArg struct.

--- StubGenerator/Program.cs
+++ StubGenerator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace StubGenerator
 {
@@ -10,5 +11,27 @@ namespace StubGenerator
         static void Main(string[] args)
         {
         }
+    }
+
+    /// <summary>
+    /// A struct containing the name, description and suggested delegate-to-be of the
+    /// argument/option
+    /// </summary>
+    public struct OptArg
+    {
+		/// <summary>
+		/// The command/option used on the CLI
+		/// </summary>
+        public string Name { get; set; }
+		
+		/// <summary>
+		/// The description from the original GIT documentation.
+		/// </summary>
+        public string Descr { get; set; }
+		
+		/// <summary>
+		/// A suggestion delegate  which can be used by nDesk;
+		/// </summary>
+        public string Deleg { get; set; }
     }
 }
